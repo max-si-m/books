f27838389c588778096c377c5f0906aa4a0b3708
Maxim Dzhuliy <mak7.dj@gmail.com>
2016-07-19 21:37:58 +0300

    Add base structure for Book model

---
+++ app/models/book.rb
@@ -0,0 +1,5 @@
+class Book < ApplicationRecord
+  validates :title, :author, presence: true
+  validates :logo, format: { with: /\Ahttps?:\/\/.*\.(?:jpe?g|png)\z/,
+    message: 'can be only image URL in jpeg or png format'  }
+end
