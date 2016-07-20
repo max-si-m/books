14763de46bfd14c79cad2aafb5523d3e27971533
Maxim Dzhuliy <mak7.dj@gmail.com>
2016-07-20 09:15:14 +0300

    Add relation between users and books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ApplicationRecord
   validates :title, :author, presence: true
   validates :logo, format: { with: /\Ahttps?:\/\/.*\.(?:jpe?g|png)\z/,
     message: 'can be only image URL in jpeg or png format'  }
+
+  belongs_to :user
 end
