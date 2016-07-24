ad7f33e20216547dadd68fac8d34ee8afde7c9f6
Maxim Dzhuliy <mak7.dj@gmail.com>
2016-07-24 15:17:02 +0300

    Add default scope for model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,6 +2,7 @@ class Genre < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: [:finders, :slugged]
 
+  default_scope -> { order(id: :desc) }
   validates :title, presence: true
 
   has_and_belongs_to_many :books
