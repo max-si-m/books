8a77dac2c4d17cc6f3292ec611f0452850607c62
Maxim Dzhuliy <mak7.dj@gmail.com>
2016-07-24 15:18:26 +0300

    Add genres manipulation to admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    resources :genres
     resources :books do
       member do
         post 'draft'
--- spec/factories/genres.rb
+++ spec/factories/genres.rb
@@ -1,4 +1,3 @@
-# FFaker don't have genres for books, I should make PR :)
 FactoryGirl.define do
   factory :genre do
     title { FFaker::Music.genre }
